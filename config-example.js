4afd691d73b1e475e5b847fbc3ebf6de6b771ef5
csgeek <samir@esamir.com>
2020-04-08 18:23:18 -0700

    Update config-example.js (#15)
    * Update config-example.js
    
    * Update config-example.js
    
    * Fix env template

--- config-example.js
+++ config-example.js
@@ -18,8 +18,9 @@ module.exports = {
     "action_log_channel": process.env.MODERATOR_LOG_CHANNEL_NAME,
     "join_log_channel" : process.env.JOIN_LOG_CHANNEL_NAME,
 
-    "excluded_trigger_channels" : [],
-
+    "excluded_trigger_channels" : process.env.EXCLUDED_CHANNELS.split(","),
+    "special_permission_flags" : process.env.SPECIAL_PERMISSION_FLAGS.split(","),
+    
     "db_name" : process.env.MYSQL_DATABASE || "beaconbot",
     "db_host" : process.env.DATABASE_HOSTNAME || "localhost",
     "db_port" : process.env.DATABASE_PORT || 3306,
