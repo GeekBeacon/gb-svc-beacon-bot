a535b22569b120b6f3f39152a35665d7cf59c82e
Toby Horn <kankuro865@gmail.com>
2020-05-03 00:26:24 -0500

    Autoroles Patch Fixed issue with assigning autoroles on join instead of on verify Changed the verify create command to send to channels with exact matches for the verify channel

--- controllers/JoinController.js
+++ controllers/JoinController.js
@@ -16,59 +16,74 @@ module.exports = {
         const joinedTime = moment(member.joinedAt).format(`HH:mm:ss`); //joined time only
         const joinLog = member.guild.channels.cache.find((c => c.name.includes(join_log_channel))); //join log channel
 
-        // Create the embed to display a new member join
-        const joinEmbed = {
-            color: 0x886CE4, //purple
-            title: `New Member`,
-            description: `<@${member.user.id}> has just joined the server!\n*${member.guild.name} now has ${member.guild.memberCount} members*`,
-            fields: [
-                {
-                    name: `User`,
-                    value: `${member.user.tag}`,
-                    inline: true,
-                },
-                {
-                    name: `Date`,
-                    value: `${joinedDate}`,
-                    inline: true,
-                },
-                {
-                    name: `Time`,
-                    value: `${joinedTime}`,
-                    inline: true,
-                },
-            ],
-            timestamp: new Date(),
-            footer: {
-                text: `User ID: ${member.user.id}`,
+        // Check the user's roles each second
+        const interval = setInterval(assignRoles, 1000);
+
+        function assignRoles() {
+
+            if(member.roles.cache.find(r => r.name === "Users")) {
+                clearInterval(interval);
+
+                const sequelize = new Sequelize(`mysql://${db_user}:${db_pass}@${db_host}:${db_port}/${db_name}`, {logging: false}); //create the sequelize connection
+                const roles = []; //create the roles array
+                const joinedDate = moment(member.joinedAt).format(`YYYY-MM-DD`); //joined date only
+                const joinedTime = moment(member.joinedAt).format(`HH:mm:ss`); //joined time only
+                const joinLog = member.guild.channels.cache.find((c => c.name.includes(join_log_channel))); //join log channel
+
+                // Create the embed to display a new member join
+                const joinEmbed = {
+                    color: 0x886CE4, //purple
+                    title: `New Member`,
+                    description: `<@${member.user.id}> has just joined the server!\n*${member.guild.name} now has ${member.guild.memberCount} members*`,
+                    fields: [
+                        {
+                            name: `User`,
+                            value: `${member.user.tag}`,
+                            inline: true,
+                        },
+                        {
+                            name: `Date`,
+                            value: `${joinedDate}`,
+                            inline: true,
+                        },
+                        {
+                            name: `Time`,
+                            value: `${joinedTime}`,
+                            inline: true,
+                        },
+                    ],
+                    timestamp: new Date(),
+                    footer: {
+                        text: `User ID: ${member.user.id}`,
+                    }
+                }
+
+                // Send the embed to the action log channel
+                joinLog.send({embed: joinEmbed});
+                
+
+                // Query the database for all of the autoroles as a select
+                sequelize.query("SELECT `role` FROM `autoroles`", {type:sequelize.QueryTypes.SELECT}).then(data => {
+
+                    // See if there are any autoroles in the db
+                    if (data) {
+                        // Find the role within the server and add it to the array
+                        data.forEach(item => {
+                            roles.push(member.guild.roles.cache.find(role => role.name === item.role));
+                        });
+
+                    // If no autoroles, just ignore assigning them
+                    } else {
+                        return;
+                    }
+                
+                }).then(() => {
+                    // Assign each role within the roles array to the user
+                    roles.forEach(role => {
+                        member.roles.add(role);
+                    });
+                });
             }
         }
-
-        // Send the embed to the action log channel
-        joinLog.send({embed: joinEmbed});
-        
-
-        // Query the database for all of the autoroles as a select
-        sequelize.query("SELECT `role` FROM `autoroles`", {type:sequelize.QueryTypes.SELECT}).then(data => {
-
-            // See if there are any autoroles in the db
-            if (data) {
-                // Find the role within the server and add it to the array
-                data.forEach(item => {
-                    roles.push(member.guild.roles.cache.find(role => role.name === item.role));
-                });
-
-            // If no autoroles, just ignore assigning them
-            } else {
-                return;
-            }
-        
-        }).then(() => {
-            // Assign each role within the roles array to the user
-            roles.forEach(role => {
-                member.roles.add(role);
-            });
-        });
-        
     }
 }
