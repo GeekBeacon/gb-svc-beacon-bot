23853774e179c2cf202065a056a58234ecc24280
Toby Horn <kankuro865@gmail.com>
2020-11-23 22:37:52 -0600

    updated config templates

--- config-example.js
+++ config-example.js
@@ -1,25 +1,6 @@
 module.exports = {
     "token" : process.env.BOT_TOKEN,
-    "prefix" : process.env.PREFIX_CHARACTER || "!",
-
     "server_id" : process.env.SERVER_ID,
-
-    "admin_role": process.env.ADMINISTRATOR_ROLE,
-    "super_role": process.env.SUPER_MODERATOR_ROLE,
-    "mod_role": process.env.MODERATOR_ROLE,
-    "mod_trainee_role": process.env.MOD_TRAINEE_ROLE,
-    "user_role": process.env.USER_ROLE,
-
-    "admin_channel": process.env.ADMINISTRATOR_CHANNEL_NAME,
-    "super_channel": process.env.SUPER_MODERATOR_CHANNEL_NAME,
-    "mod_channel": process.env.MODERATOR_CHANNEL_NAME,
-    "super_log_channel": process.env.SUPER_MODERATOR_LOG_CHANNEL_NAME,
-    "action_log_channel": process.env.MODERATOR_LOG_CHANNEL_NAME,
-    "join_log_channel" : process.env.JOIN_LOG_CHANNEL_NAME,
-
-    "excluded_trigger_channels" : process.env.EXCLUDED_CHANNELS.split(","),
-    "url_role_whitelist" : process.env.URL_ROLE_WHITELIST.split(","),
-    "special_permission_flags" : process.env.SPECIAL_PERMISSION_FLAGS.split(","),
     
     "db_name" : process.env.MYSQL_DATABASE || "beaconbot",
     "db_host" : process.env.DATABASE_HOSTNAME || "localhost",
