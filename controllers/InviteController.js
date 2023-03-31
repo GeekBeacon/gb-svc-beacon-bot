83da404020379207a1d1a0f2944b5d7c7f7d7b67
Kankuro <kankuro@geekbeacon.org>
2023-03-31 18:45:24 -0500

    Added more clarity

--- controllers/InviteController.js
+++ controllers/InviteController.js
@@ -10,12 +10,12 @@ module.exports = {
         const adminChannel = invite.guild.channels.cache.find((c => c.name.includes(client.settings.get("admin_channel_name")))); //admin channel
 
         // If the invite code is using the vanity url, then ignore it
-        if(invite.code === invite.vanityURLCode) {
+        if(invite.code === invite.guild.vanityURLCode) {
             return;
         } else {
             invite.delete(`Auto deleted invite, please use vanity url or discord.geekbeacon.org!`).then(() => {
                 if(invite.guild.vanityURLCode) {
-                    adminChannel.send(`${invite.inviter}, I have deleted your recently created invite. Please use https://discord.gg/${invite.guild.vanityURLCode} or https://discord.geekbeacon.org to invite people to the server!`);
+                    adminChannel.send(`${invite.inviter}, I have deleted your recently created invite. Please use https://discord.gg/${invite.guild.vanityURLCode} or https://discord.geekbeacon.org to invite people to the server! \n\nIf this was created from an event, either copy the event link or copy the event id and append \`?event={event id}\` to the vanity url.`);
                 } else {
                     adminChannel.send(`${invite.inviter}, I have deleted your recently created invite. Please use https://discord.geekbeacon.org to invite people to the server!`);
                 }
